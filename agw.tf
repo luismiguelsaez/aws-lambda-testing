1803d864b11aca1825e4efd37535524722a02d76
luismiguelsaez <luismiguelsaez83@gmail.com>
2020-11-30 15:31:01 +0100

    Added integration comment

--- agw.tf
+++ agw.tf
@@ -24,6 +24,8 @@ resource "aws_api_gateway_integration" "application" {
   rest_api_id = aws_api_gateway_rest_api.main.id
   resource_id = aws_api_gateway_resource.application.id
   http_method = aws_api_gateway_method.application_get.http_method
+  # When backend application is a lambda function, integration_http_method must be POST
+  # https://github.com/amazon-archives/aws-apigateway-importer/issues/9#issuecomment-129651005
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.example.invoke_arn
