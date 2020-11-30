982fc48b0077bb16b5838a750031a21f92fab9ea
luismiguelsaez <luismiguelsaez83@gmail.com>
2020-11-30 09:02:39 +0100

    Added VERSION variable

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # aws-lambda-testing
+
+### Deploy
+
+```
+TF_VAR_VERSION=v1.0.0 terraform apply
+```
--- vars.tf
+++ vars.tf
@@ -3,3 +3,8 @@ variable "environment" {
     type = string
     default = "testing"
 }
+
+variable "VERSION" {
+    type = string
+    default = "v1.0.0"
+}
